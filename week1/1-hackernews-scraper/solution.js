7794b1fe7f9a0b4848f0fe732296d2e47f6f87b4
Ivan Latunov <xswordsx@gmail.com>
2014-10-18 15:39:18 +0300

    Add run file

---
+++ week1/1-hackernews-scraper/solution.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var subscriber = require('./subscriber');
+var notifier = require('./notifier');
+
+var db = new Subscriber();
+
+var app = express();
+
+app.use('/subscribe', bodyParser.json());
+
+app.post('/subscribe', function(req, res) {
+	var subsciberInfo = db.subscribe(req.body);
+	res.json(subsciberInfo.subscriber);
+	res.status(201).end();
+});
+
+app.post('/unsubscribe', function(req, res) {
+	req.on('data', function(data){
+		db.unsubscribe(data.toString());
+	res.status(201).end();
+	});
+});
+
+app.get('/listSubscribers', function(req, res) {	
+	res.status(201).end(db.listSubscribers().join('\n'));
+});
+
+
+var port = Number(process.argv[2]) || 8080;
+console.log('Server listening on port ' + port);
+app.listen(port);
