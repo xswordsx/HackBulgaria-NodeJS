9b11af8945fe8c94098fef8d1b2d873b5566412e
Ivan Latunov <xswordsx@gmail.com>
2014-11-11 21:35:12 +0200

    Add userModel to main app

--- week5/1-Passport-Basics/app.js
+++ week5/1-Passport-Basics/app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var passport = require('passport');
 var path = require('path');
+var userModel = require('./userModel');
 
 var config = require(path.resolve(__dirname + '/config.json'));
+
+app.use(express.static(__dirname + '/public'));
+
 
 app.listen(config.port);
 console.log("Express server running on", config.host + ":" + config.port);
