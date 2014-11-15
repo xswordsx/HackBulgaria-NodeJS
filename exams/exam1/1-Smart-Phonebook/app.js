f914e4a1deaaf4f65a955faa7e2ac0198b63bfeb
Ivan Latunov <xswordsx@gmail.com>
2014-11-15 21:54:07 +0200

    Implement CR[u]D for contacts

--- exams/exam1/1-Smart-Phonebook/app.js
+++ exams/exam1/1-Smart-Phonebook/app.js
@@ -1,28 +1,101 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var q = require('q');
+
 var config = require('./config.json');
 var Contact = require('./models/contact');
 
-app.post('/contact', function (req, res) {
+app.use(express.static(__dirname + '/test/'));
+app.use('/contact', bodyParser.urlencoded({
+	extended: true
+}));
 
+app.post('/contact', function (req, res) {
+	var contact = new Contact.model({
+		phoneNumber: req.body.phoneNumber
+	});
+	contact.save(function(err) {
+		if(err) {
+			res.status(400).end("Could not save number: " + err);
+		} else {
+			var result = "Phone number: " + contact.phoneNumber;
+			result += "\n" + "ID: " + contact.personIdentifier;
+			res.status(200).end(contact._id);
+		}
+	});
 });
 
 app.get('/contacts', function (req, res) {
-  res.json({});
+	var query = {};
+	query.map = function() {
+		emit(this._id, {
+			phoneNumber: this.phoneNumber,
+			personIdentifier: this._id
+		});
+	};
+	query.reduce = function (key, value) {
+		return value;
+	};
+	Contact.model.mapReduce(query, function (err, contacts) {
+		if(err) {
+			res.status(400).end("Something went wrong: " + err);
+		} else {
+			var mappedResult = contacts.map(function (contact){
+				return contact.value;
+			});
+			res.status(200).json(mappedResult);
+		}
+	});
 });
 
 app.get('/contact/:id', function (req, res) {
-  var currentContact = Contact.get(req.params.id);
-  if(currentContact) {
-    res.status(200).json(currentContact);
-  } else {
-    res.status(404).end("Contact does not exist");
-  }
+	Contact.model.findOne({_id: req.params.id}, function (err, contact) {
+		if(err) {
+			res.status(404).end("User not found");
+		} else {
+			res.status(200).json(contact);
+		}
+	});
 });
 
 app.delete('/contact/:id', function (req, res) {
-
+	Contact.model.findByIdAndRemove(req.params.id, function (err) {
+		if(err) {
+			res.status(400).end("Could not remove contact with id: " + req.params.id);
+		} else {
+			res.status(302).end("Removed contact with id: " + req.params.id);
+		}
+	});
 });
 
-app.listen(config.port);
+var httpServer = app.listen(config.port);
 console.log("Smart phonebook server listening on", config.host + ":" + config.port);
+Contact.connect()
+	.then(function(result){
+		console.log("Connected to database:", result);
+	})
+	.fail(mannerly_close);
+
+function mannerly_close(err) {
+	Contact.disconnect()
+		.then(function(succ){
+			console.log("Connection to database closed.");
+		})
+		.fail(function(err){
+			console.log("Unable to close connection to databse:", err.toString());
+		})
+		.finally(function (){
+			if(err) {
+				console.error(err.toString());
+			}
+			console.log("Exiting node...");
+			process.exit(0);
+		});
+}
+
+process.on("SIGINT", mannerly_close);
+process.on("exit", mannerly_close);
+process.on("uncaughtException", function (err){
+	mannerly_close(err);
+});
